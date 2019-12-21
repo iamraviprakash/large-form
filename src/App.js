232f1229dfc66794fdf5c9a7e5bce881ff96994e
shahchaitanya <shahchaitu4@gmail.com>
2019-12-21 15:15:00 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,103 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import _ from "lodash";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const elemnets = 1000;
+
+const FIELDCOLUMN = [
+  { label: "Name", fieldKey: "name" },
+  { label: `Father's Name`, fieldKey: "fName" },
+  { label: `Mother's Name`, fieldKey: "mName" },
+  { label: `Email`, fieldKey: "email" },
+  { label: `Contact No`, fieldKey: "contactNum" },
+  { label: "City", fieldKey: "city" },
+  { label: "Country", fieldKey: "country" },
+
+  { label: "Delete", fieldKey: "delete" }
+];
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      userList: Array.apply(null, Array(elemnets)).map(() => {
+        return {};
+      })
+    };
+  }
+
+  onInputFieldChange = ({ index, value, fieldKey }) => {
+    const userList = this.state.userList;
+    userList[index][fieldKey] = value;
+    this.setState({});
+  };
+
+  addUser = () => {
+    const userList = this.state.userList;
+    userList.splice(0, 0, {});
+    this.setState({ userList });
+  };
+
+  onClickDelete = ({ index }) => {
+    const userList = this.state.userList;
+    userList.splice(index, 1);
+    this.setState({ userList });
+  };
+
+  render() {
+    const { userList } = this.state;
+
+    return (
+      <div className="container">
+        <div className="header">
+          <div className="headerText">{`Users`}</div>
+          <div className="button" onClick={this.addUser}>{`Add User`}</div>
+        </div>
+        <div className="scrollContainer">
+          <div className="listContainer">
+            <div className={"row headerRow"}>
+              {_.map(FIELDCOLUMN, ({ label }) => {
+                return <div className={"headerColumn"}>{label}</div>;
+              })}
+            </div>
+            {_.map(userList, (user, index) => {
+              return (
+                <div className="row">
+                  {_.map(FIELDCOLUMN, ({ label, fieldKey }, index) => {
+                    return (
+                      <div className={"headerColumn"}>
+                        {fieldKey != "delete" ? (
+                          <input
+                            type="text"
+                            className={"inputField"}
+                            value={user[fieldKey] || ""}
+                            onChange={e =>
+                              this.onInputFieldChange({
+                                value: e.target.value,
+                                index,
+                                fieldKey
+                              })
+                            }
+                          ></input>
+                        ) : (
+                          <div
+                            className={"delete"}
+                            onClick={() => this.onClickDelete({ index })}
+                          >
+                            {"Delete"}
+                          </div>
+                        )}
+                      </div>
+                    );
+                  })}
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
